5cc7327f2b664d54e50ee1dde112850657a5b2f4
Ferno <fernandeskj007@gmail.com>
2023-03-03 18:31:07 +0530

    second

--- src/Component2/Category.js
+++ src/Component2/Category.js
@@ -3,6 +3,8 @@ function Category(props) {
   return (
     <div>
         <button onClick={()=>props.filter("breakfast")}>Breakfast</button>
+        <button onClick={()=>props.filter("lunch")}>Lunch</button>
+        <button onClick={()=>props.filter("shakes")}>Shakes</button>
     </div>
   )
 }
